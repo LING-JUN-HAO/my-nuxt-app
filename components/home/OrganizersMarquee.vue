1f156d9b63943d5ee2d6862d45ac5fef1267c7e6
Ling,Jun- Hao <Antonio.Ling@hepiuscare.com.tw>
2025-10-16 10:27:22 +0800

    feat: 測試 0

--- components/home/OrganizersMarquee.vue
+++ components/home/OrganizersMarquee.vue
@@ -61,10 +61,6 @@ const ORGANIZERS = [
           format="webp"
           width="150"
           class="aspect-[5/4] w-[96px] transition-opacity duration-300 sm:w-[120px] lg:w-[150px] lg:group-hover:opacity-0"
-          @error="
-            (event) =>
-              ((event.target as HTMLImageElement).src = '/images/test.jpg')
-          "
         />
         <NuxtImg
           :src="organizer.hoverLogo"
@@ -72,10 +68,6 @@ const ORGANIZERS = [
           format="webp"
           width="150"
           class="absolute inset-0 aspect-[5/4] w-[96px] opacity-0 transition-opacity duration-300 sm:w-[120px] lg:w-[150px] lg:group-hover:opacity-100"
-          @error="
-            (event) =>
-              ((event.target as HTMLImageElement).src = '/images/test.jpg')
-          "
         />
         <span
           v-if="organizer.consecutiveText !== ''"
